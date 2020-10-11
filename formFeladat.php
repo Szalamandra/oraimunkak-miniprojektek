04d88879e47a245d70cbbf1f08a2904f878296db
Szalamandra <Szalamandra@users.noreply.github.com>
2020-10-11 12:49:37 +0200

    félkész állapot

---
+++ formFeladat.php
@@ -0,0 +1,9 @@
+
+ <?php
+
+/* Zsigmond Réka, 2020-10-10 */
+
+echo file_get_contents('templatesFormFeladat/head.tpl');
+echo file_get_contents('templatesFormFeladat/index.tpl');
+echo file_get_contents('templatesFormFeladat/foot.tpl');
+
